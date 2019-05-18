1a8f8fb1b0b72db1e5ee15f853c161c13facfec2
ThomasMartinUK <tmuk@protonmail.com>
2019-05-18 17:14:11 +0100

    Added Pokemon images and API call to get Pokemon type

--- pokedex/src/Components/PokemonList.js
+++ pokedex/src/Components/PokemonList.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import Axios from 'axios';
 
 class PokemonList extends React.Component {
     state = {
         name: '',
         imageUrl: '',
-        pokemonIndex: ''
+        pokemonIndex: '',
+        type: ''
+    }
+
+    componentDidMount(){
+        const { name, url } = this.props
+        const pokemonIndex = url.split('/')[url.split('/').length - 2];
+        // const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`
+
+        this.pokeType();
+
+        this.setState({
+            name, 
+            imageUrl, 
+            pokemonIndex,
+        });
+    }
+
+    pokeType = async () => {
+        const response = await Axios.get(this.props.url);
+        console.log(response.data.types[0].type.name);
+        this.setState({
+            type: response.data.types[0].type.name
+        })
     }
 
     render(){
-        const { name, url } = this.props
+        
 
         return(
             <div className = "ui card">
+                <div className="image">
+                    <img src= {this.state.imageUrl} alt="pokemon"/>
+                </div>
                 <div className = "content">
                     <div className = "header">
-                        <div>{name}</div>
+                        <div>{this.state.pokemonIndex}</div>
+                    </div>
+                    <div className = "meta">
+                        <div>{this.state.name}</div>
+                        <div>{this.state.type}</div>
                     </div>
                 </div>
             </div>
